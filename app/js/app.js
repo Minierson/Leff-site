344aa47af97d6854f1cfa8b11f8b45e640d98077
Maciej Kienig <maciejkienig@gmail.com>
2017-06-01 16:25:26 +0200

    new changes
    Adding some js and more html elements

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,6 @@
 /* main js file */ 
+
+/********* activating the hamburger menu **************/
 var navButton = document.querySelector('.btn-nav');
 navButton.addEventListener('click', function(){
     var cl = this.getAttribute('class');
@@ -9,3 +11,34 @@ navButton.addEventListener('click', function(){
     }
 
 });
+
+/********** changing logo colors  *****************/
+
+var allegro = document.getElementById('allegro');
+var ebay = document.getElementById('ebay');
+var leff = document.getElementById('leff-hurt');
+/* allegro */
+allegro.addEventListener('mouseover', function(){
+    this.setAttribute('src', '/img/allegro_col.png');
+});
+allegro.addEventListener('mouseout', function(){
+    this.setAttribute('src', '/img/allegro_bw.png');
+});
+
+/* ebay*/
+ebay.addEventListener('mouseover', function(){
+    this.setAttribute('src', '/img/ebay_col.png');
+});
+ebay.addEventListener('mouseout', function(){
+    this.setAttribute('src', '/img/ebay_bw.png');
+});
+
+/** Leff hurt */
+leff.addEventListener('mouseover', function(){
+    this.setAttribute('src', '/img/leff-hurt_col.png');
+});
+leff.addEventListener('mouseout', function(){
+    this.setAttribute('src', '/img/leff-hurt_bw.png');
+});
+
+
